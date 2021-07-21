9b4ae6dacaad54f8f6f66a40da315673b74438be
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-07-21 19:38:29 +0530

    Update logomaker.py

--- YoneRobot/modules/logomaker.py
+++ YoneRobot/modules/logomaker.py
@@ -20,13 +20,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./YoneRobot/resources/blackbg.jpg')
+    img = Image.open('./MissVisa/resources/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./YoneRobot/resources/Chopsic.otf", 330)
+    font = ImageFont.truetype("./MissVisa/resources/Chopsic.otf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -34,7 +34,7 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
-    fname2 = "LogoByYone.png"
+    fname2 = "LogoByMissVisa.png"
     img.save(fname2, "png")
     await tbot.send_file(event.chat_id, fname2, caption="Made By MissVisa")
     if os.path.exists(fname2):
